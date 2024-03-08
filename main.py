13285fdb7ec2f44be5d00c72559c56a2c5f13b46
senarobin <140357675+senarobin@users.noreply.github.com>
2024-03-08 20:16:14 +0000

    mudancas

--- main.py
+++ main.py
@@ -16,28 +16,63 @@ def q2():
     2. Verificação de Número Par/Ímpar: Crie um programa que pede ao usuário 
     um número e imprima se ele é par ou ímpar.
     """
-    print("par ")
+    n = int(input('Digite um número: '))
 
+    if n % 2 == 0:
+        print('par')
+    else:
+        print('ímpar')
 def q3():
     """
     3. Calculadora Simples: Faça uma calculadora que pede ao usuário dois 
     números e uma operação (+, -, *, /) e imprima o resultado dessa operação.
     """
-    pass
+    n = float(input('Digite um número: '))
+    n_dois = float(input('Digite outro número: '))
+
+    op = input('Digite uma operação. Ex.: +, -, *, /: ')
+
+    if op == '+':
+        print(n + n_dois)
+    elif op == '-':
+        print(n - n_dois)
+    elif op == '*':
+        print(n * n_dois)
+    elif op == '/':
+        print(n / n_dois)
 
 def q4():
     """
     4. Maior de Três Números: Escreva um programa que solicita três números 
     ao usuário e imprima o maior dentre eles.
     """
-    pass
+    n = float(input('Digite um número: '))
+    n_dois = float(input('Digite outro número: '))
+    n_tres = float(input('Digite mais um número: '))
+
+    if n > n_dois and n > n_tres:
+        print(n)
+    elif n_dois > n and n_dois > n_tres:
+        print(n_dois)
+    elif n_tres > n and n_tres > n_dois:
+        print(n_tres)
+
 
 def q5():
     """
     5. Classificação de Idade: Peça a idade do usuário e imprima a classificação
     em "Criança" (0-12), "Adolescente" (13-19), "Adulto" (20-59) ou "Idoso" (60+).
     """
-    pass
+    idade = int(input('Digite sua idade: '))
+
+    if idade >= 0 and idade <= 12:
+        print('Criança')
+    elif idade >= 13 and idade <= 19:
+        print('Adolescente')
+    elif idade >= 20 and idade <= 59:
+        print('Adulto')
+    elif idade > 60:
+        print('Idoso')
 
 def q6():
     """
@@ -45,7 +80,19 @@ def q6():
     lados e verifique se eles podem formar um triângulo. Se sim, determine 
     se é um triângulo equilátero, isósceles ou escaleno.
     """
-    pass
+    medida_um = int(input('Digite o primeiro comprimento: '))
+    medida_dois = int(input('Digite o segundo comprimento: '))
+    medida_tres = int(input('Digite o terceiro comprimento: '))
+
+    if medida_um + medida_dois < medida_tres or medida_dois + medida_tres < medida_um or medida_um + medida_tres < medida_dois:
+        print('Inválido')
+    else:
+        if medida_um == medida_dois == medida_tres:
+            print('Equilátero')
+        elif medida_um == medida_dois != medida_tres or medida_um == medida_tres != medida_dois or medida_dois == medida_tres != medida_um:
+            print('Isósceles')
+        elif medida_um != medida_dois != medida_tres:
+            print('Escaleno')
 
 def q7():
     """
@@ -53,8 +100,20 @@ def q7():
     de 0 a 100 em uma escala de conceitos: 
     A (90-100), B (80-89), C (70-79), D (60-69), E (50-59).e F (0-49)
     """
-    pass
+    nota = float(input('Digite sua nota de 0 a 100: '))
 
+    if nota >= 90 and nota <=100:
+        print('A')
+    elif nota >= 80 and nota <= 89:
+        print('B') 
+    elif nota >= 70 and nota <= 79:
+        print('C')
+    elif nota >= 60 and nota <= 69:
+        print('D') 
+    elif nota >= 50 and nota <= 59:
+        print('E')
+    elif nota >= 0 and nota <= 49:
+        print('F') 
 def q8():
     """
     8. Validação de Login: Crie um programa que pede ao usuário um nome 
